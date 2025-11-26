0825ea779331b0dabf3187743327abe9cb0c8aac
BernathTamas7 <bernathtamasx@gmail.com>
2025-11-26 13:54:19 +0100

    szekfoglalo felulet teljesen kesz

--- src/main/java/com/cinema_ticket/ui/SeatPickPanel.java
+++ src/main/java/com/cinema_ticket/ui/SeatPickPanel.java
@@ -15,27 +15,6 @@ public class SeatPickPanel extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JPanel gridPanel = new JPanel(new GridLayout(10,10));
-        for(int i = 1; i < 11; i++){
-            for(int j = 1; j < 11; j++){
-                JButton button = new JButton();
-                if(seatService.getSeats().getSeat(i, j).getStatus().equals(SeatStatus.RESERVED)){
-                    button.setBackground(Color.GRAY);
-                }else{
-                    button.setBackground(Color.GREEN);
-                }
-                gridPanel.add(button);
-            }
-        }
-
-        //felso panel letrehozasa es kep beszurasa es meretezese
-        ImageIcon icon = new ImageIcon("data/vaszon_kep.png");
-        Image scaled = icon.getImage().getScaledInstance(800, 80, Image.SCALE_SMOOTH);
-        JLabel imageLabel = new JLabel(new ImageIcon(scaled));
-        imageLabel.setPreferredSize(new Dimension(800, 80));
-        JPanel upperPanel = new JPanel();
-        upperPanel.add(imageLabel);
-
         //also panel letrehozasa
         JPanel downPanel = new JPanel();
         //also panelhez jegy kep hozzaadasa
@@ -49,10 +28,55 @@ public class SeatPickPanel extends JFrame{
         String printOut = "" + ticketNumber;
         JLabel ticketNumberLabel = new JLabel(printOut);
         downPanel.add(ticketNumberLabel);
+
+        JPanel gridPanel = new JPanel(new GridLayout(10,10));
+        for(int i = 1; i < 11; i++){
+            for(int j = 1; j < 11; j++){
+                JButton button = new JButton();
+                if(seatService.getSeats().getSeat(i, j).getStatus().equals(SeatStatus.RESERVED)){
+                    button.setBackground(Color.GRAY);
+                }else{
+                    button.setBackground(Color.GREEN);
+                }
+                button.addActionListener(e ->{
+                    JButton tmp = (JButton) e.getSource();
+                    if(tmp.getBackground().equals(Color.GREEN)){
+                        tmp.setBackground(Color.BLUE);
+                        ticketNumber++;
+                    }
+                    else if(tmp.getBackground().equals(Color.BLUE)){
+                        tmp.setBackground(Color.GREEN);
+                        ticketNumber--;
+                    }
+                    ticketNumberLabel.setText(""+ticketNumber);
+                });
+                gridPanel.add(button);
+            }
+        }
+
+        //felso panel letrehozasa es kep beszurasa es meretezese
+        ImageIcon icon = new ImageIcon("data/vaszon_kep.png");
+        Image scaled = icon.getImage().getScaledInstance(800, 80, Image.SCALE_SMOOTH);
+        JLabel imageLabel = new JLabel(new ImageIcon(scaled));
+        imageLabel.setPreferredSize(new Dimension(800, 80));
+        JPanel upperPanel = new JPanel();
+        upperPanel.add(imageLabel);
+
+        
+
+        //jobb oldali panel elkeszitese, gombok rarakasa
+        JPanel eastPanel = new JPanel(new GridLayout(2, 1));
+        JButton backButton = new JButton("Back");
+        JButton confirmButton = new JButton("Confirm");
+        eastPanel.add(backButton);
+        eastPanel.add(confirmButton);
+
+        
+        
         
 
 
-
+        this.add(eastPanel, BorderLayout.EAST);
         this.add(downPanel, BorderLayout.SOUTH);
         this.add(upperPanel, BorderLayout.NORTH);
         this.add(gridPanel, BorderLayout.CENTER);
